94c4cecd5de31cb34d2672b751894fede9684672
Maarten <36227274+fiexer@users.noreply.github.com>
2022-01-05 16:29:12 +0100

    Adjustments for Shiden

--- scripts/rpc-shiden.sh
+++ scripts/rpc-shiden.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-FILE="astar-rpc"
-SERVICE="astar-rpc"
+FILE="shiden-rpc"
+SERVICE="shiden-rpc"
 
 function usage {
    cat << EOF
-Usage: rpc.sh -auth <ngrok auth token>
+Usage: rpc.sh -name <node name> -auth <ngrok auth token>
 
 Runs RPC node from docker with ngrok tunnel setting
 EOF
@@ -30,7 +30,7 @@ sudo docker pull staketechnologies/astar-collator
 docker run -m 5G --name Shiden -p 30334:30334 -p 30333:30333 -p 9933:9933 -p 9944:9944 \
 -v "/var/lib/astar/shiden-db:/data" \
 -u $(id -u ${USER}):$(id -g ${USER}) -d --network=host staketechnologies/astar-collator \
-astar-collator --name NAME --base-path /data --port 30333 --rpc-port 9933 --unsafe-rpc-external --unsafe-ws-external --pruning archive \
+astar-collator --name $NAME --base-path /data --port 30333 --rpc-port 9933 --unsafe-rpc-external --unsafe-ws-external --pruning archive \
 --state-cache-size 1 --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
 -l evm=debug,ethereum=debug,rpc=debug \
 
